a0aa241cb140cb3576e39be1db7cc08cb389543b
lsudupe <sudupelaura@gmail.com>
2021-10-23 16:48:50 +0200

    script to load all the packages needed to work

---
+++ packages.R
@@ -0,0 +1,12 @@
+## SCRIPT: Packages to load
+
+## 23.10.21 Laura Sudupe , git @lsudupe
+
+## a list of packages to load to work with spatial data 
+
+library(Seurat) ## single cell workflow toolkit
+library(ggplot2) ## 
+library(tidyverse) ## ubiquitous - data manipulatation toolkit
+library(Matrix) ## read in a .mtx file
+library(data.table)
+library(dplyr)
