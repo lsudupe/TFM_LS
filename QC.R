1a66d7f4f406d3cc72cb9f14c4334bca58a5e1b4
lsudupe <sudupelaura@gmail.com>
2021-10-30 10:30:19 +0200

    before merge them

--- QC.R
+++ QC.R
@@ -5,31 +5,31 @@
 #Libraries-------------------------------------------------
 source("packages.R")
 
-#read objects
+#Read objects
 sham <- readRDS("./objects/initial/sham.sp.rds")
 day1.1 <- readRDS("./objects/initial/day1.1.sp.rds")
 day1.2 <- readRDS("./objects/initial/day1.2.sp.rds")
 day7.1 <- readRDS("./objects/initial/day7.2.sp.rds")
 day7.2 <- readRDS("./objects/initial/day7.2.sp.rds")
 
-# Create sample column
-sham@meta.data$sample <- NA
-metadata$sample[which(str_detect(metadata$protocol, "sham"))] <- "sham"
-metadata$sample[which(str_detect(metadata$protocol, "day1"))] <- "day 1"
-metadata$sample[which(str_detect(metadata$protocol, "day7"))] <- "day 7"
+#Add number of genes per UMI for each cell to metadata
+sham$log10GenesPerUMI <- log10(sham$nFeature_Spatial) / log10(sham$nCount_Spatial)
+day1.1$log10GenesPerUMI <- log10(day1.1$nFeature_Spatial) / log10(day1.1$nCount_Spatial)
+day1.2$log10GenesPerUMI <- log10(day1.2$nFeature_Spatial) / log10(day1.2$nCount_Spatial)
+day7.1$log10GenesPerUMI <- log10(day7.1$nFeature_Spatial) / log10(day7.1$nCount_Spatial)
+day7.2$log10GenesPerUMI <- log10(day7.2$nFeature_Spatial) / log10(day7.2$nCount_Spatial)
 
-sp.list <- c(sham,day1.1,day1.2,day7.1,day7.2)
+# Compute percent mito ratio
+sham <- PercentageFeatureSet(sham, "^mt-", col.name = "percent_mito")
+day1.1 <- PercentageFeatureSet(day1.1, "^mt-", col.name = "percent_mito")
+day1.2 <- PercentageFeatureSet(day1.2, "^mt-", col.name = "percent_mito")
+day7.1 <- PercentageFeatureSet(day7.1, "^mt-", col.name = "percent_mito")
+day7.2 <- PercentageFeatureSet(day7.2, "^mt-", col.name = "percent_mito")
 
-# Add number of genes per UMI for each spot to metadata
-for (i in 1:length(sp.list)) {
-  sp.list[[i]]$log10GenesPerUMI <- log10(sp.list[[i]]$nFeature_Spatial) / log10(sp.list[[i]]$nCount_Spatial)
-}
-# Create sample column
-for (i in 1:length(sp.list)) {
-  sp.list[[i]]@meta.data$sample <- NA
-}
-sp.list[[1]]@meta.data$sample <- "sham"
-sp.list[[2]]@meta.data$sample <- "day1.1"
-sp.list[[3]]@meta.data$sample <- "day1.2"
-sp.list[[4]]@meta.data$sample <- "day7.1"
-sp.list[[5]]@meta.data$sample <- "day7.1"
+##add sample column
+sham@meta.data$sample <- "sham"
+day1.1@meta.data$sample <- "day1.1"
+day1.2@meta.data$sample <- "day1.2"
+day7.1@meta.data$sample <- "day7.1"
+day7.1@meta.data$sample <- "day7.2"
+
