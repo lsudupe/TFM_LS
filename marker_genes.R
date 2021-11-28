f6c16141af0d9c1b197bafce4c6b45bc531efba8
lsudupe <sudupelaura@gmail.com>
2021-11-28 22:20:15 +0300

    literature marker genes related to heart landscape

---
+++ marker_genes.R
@@ -0,0 +1,53 @@
+## SCRIPT: Marker genes 
+
+## 28.11.21 Laura Sudupe , git @lsudupe
+
+###List of heart landscape marker genes
+
+fibroblast <- c("Col1a1","Col3a1","Ddr2","Ly6a","Thy1","S100a4","Tcl21",
+                "Crispld2","Ogn","Islr","Pdgfra","Dcn","Gsn")
+circulation.fibroblast <- c("Cthrc1","Ddah1","Fmod","Lox","Comp","Ptn",
+                            "Gsn","Pdgfra","Ddr2","Tcf21","P4hb","Acta2",
+                            "Postn","Ckap4","Fstl1")
+meshotelial <- c("Msln","Wt1","Bnc1")
+pericytes <- c("Rgs5","Abcc9","Kcnj8")
+adipocytes <- c("Gpam","Fasn","Lep")
+neuronal <- c("Plp1","Nrxn1","Nrxn3")
+myeloid <- c("Cd14","C1qa","Cd68")
+lymphoid <- c("Cd8a","Il7r","Cd40lg","Cd19","Cd11b","Th1","Th2","Th17","Treg")
+cardiomyocite <-c("Tnnt2","Tpma","Tnnc1","Tnni3","Myoz2","Actc1","Ryr2",
+                  "Ttn","Mhrt","Tnnc1","Pde4d")
+
+endothelial <- c("Lye61","Lims2","Rgcc","C1qtnf9","Cyyr1","Vwf","Pecam1",
+                 "Cdh5","Sele","Icam1","Icam2","Vcam1","Flt1","Flk1","Tie1",
+                 "Tie2","Eng","Emcn")
+
+smooth.muscle <- c("Tagln","Myh11","Olfr558","Lmod1","Nrip2","Acta2")
+B.cell <- c("Cd79a","Ly6d","H2-DMb2","Cd79b","Ms4a1","Ccr7","Cd19",
+            'Cd79b','Mef2c','Fcer2a','Cd74','Ccr7','Cd24a','Siglecg',
+            'Cd19','Cd37','Scd1','Pax5','Cd69','Rel','Myc','Cd22','Cxcr5',
+            'Irf4', 'Syk','Btla','Cd38','Cxcr4','Cx3cr1','Cd86','Ccl12','Tgfb1',
+            'Foxo1','Cd274','Csf1r','Ly6a','Ly6d','Fcmr','Jund')
+
+T.cell <- c('Cd8b1','Cd3d','Cd3g','Cd8a','Lat','Cd3e','Il7r','Thy1','Cd28',
+            'Ly6c1','Bcl2','Lck','S1pr1','Cd4','Ccr7','Cd27','Jun','Cd69',
+            'Cd5','Icos','Zap70','Ly6c2','Il6ra','Dapl1','Tcf7','Cd247')
+DC.like.cells <- c("Lgals3", "Napsa","Plbd1", "Ccr2","Rnase6","Naaa","Plac8",
+                   "Ifitm6","Ear2","H2afy","Ly6c1")
+Schwann.cells <- c("Kcna1","Cnp", "Plp1","Gfra3","Gpr3711","Nm1","Aspa",
+                   "Cd59a","Stmn1","Tagin")
+Macrophagues <- c("Hdc", "Dab2","Adgre1","Mg12","Mrc1","Hogd","P2ry6",
+                  "C3ar1","F13a1","Maf","Ms4a7")
+neutrophils <- c('Il1b','Csf3r','Cxcl2','Ccr1','Ccl6','Cxcr2','S100a9',
+                 'C5ar1','S100a8','Clec4e','Retnlg','Ifitm1','Mcl1','Cd33',
+                 'Il1r2','S100a11','Lrg1','Ifitm2','Nlrp3','Entpd1','Selplg',
+                 'Nfkbiz','Mmp9','Cd14','Fcer1g','Tgfbi','Ifitm3','Cd84',
+                 'Ccrl2','Hif1a','Cxcr4','Chil3','Itgam','Itgal','Tlr2',
+                 'Ccl9','Ccr2','Csf1r','Cx3cr1','Cd68')
+dentritic <- c('Cd209a','Ifi30','Cd209d','Tnfsf9','Cd74','Irf8','Il1b',
+               'Ccr2','Nfkb1','Irf5','Klrd1','Ccl9','Socs3','Selplg',
+               'Fos','Cd68','H2-T23','Myd88')
+granulocytes = c("S100a8","S100a9","Slpi","Csf3r","Hcar2","Lmnb1","Retnlg",
+                 "Clec4d","Hp","Hdc")
+
+
