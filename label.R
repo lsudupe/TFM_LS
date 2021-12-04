4c7b85c9d926b7dc4932e596ed29f8baed617973
lsudupe <sudupelaura@gmail.com>
2021-12-04 19:30:07 +0300

    order change

--- label.R
+++ label.R
@@ -18,12 +18,12 @@ Seurat::Idents(object = integrated) <- integrated@meta.data[["ident"]]
 integrated.label <- RenameIdents(object = integrated, `0` = "CD",
                                     `1` = "CD",`2` = "Immune cells",`3` = "EC",
                                     `4` = "FB",`5` = "CD",`6` = "FB")
+integrated.label$ident <- (integrated.label@active.ident)
+
 saveRDS(integrated.label, "./objects/processed/integrated.label.rds")
 
 
 ###Plot 
-
-integrated.label$ident <- (integrated.label@active.ident)
 
 pdf(file.path("./results/clusters",filename = "umap_r0.4_integrated_label.pdf"))
 DimPlot(integrated.label, group.by = c("ident"), label = T) + ggtitle("UMAP_r0.4")
