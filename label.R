af14efac90e3c17fb7a19a00b61308522cec30fe
lsudupe <sudupelaura@gmail.com>
2021-12-10 16:12:25 +0300

    last details

--- label.R
+++ label.R
@@ -5,12 +5,12 @@
 #Libraries-------------------------------------------------
 source("packages.R")
 
-###Read data
+#Read data
 
 integrated <- readRDS("./objects/processed/integrated.sct.rds")
 
 
-###Label the clusters
+#Label the clusters
 
 integrated$ident <- integrated@meta.data[["seurat_clusters"]]
 Seurat::Idents(object = integrated) <- integrated@meta.data[["ident"]]
@@ -23,7 +23,7 @@ integrated.label$ident <- (integrated.label@active.ident)
 saveRDS(integrated.label, "./objects/processed/integrated.label.rds")
 
 
-###Plot 
+#Plot 
 
 pdf(file.path("./results/clusters",filename = "umap_r0.4_integrated_label.pdf"))
 DimPlot(integrated.label, group.by = c("ident"), label = T) + ggtitle("UMAP_r0.4")
